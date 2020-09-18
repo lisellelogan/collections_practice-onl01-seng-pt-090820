2804acd575690560c2f33f5134a0a761f0962266
Liselle Logan <liselle.logan@gmail.com>
2020-09-18 17:48:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(descend)
+  descend.sort.reverse
+end
+
+def sort_array_char_count(character)
+  character.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(swap)
+  swap[1], swap[2] = swap[2], swap[1]
+  swap
+end
+
+def reverse_array(reversed)
+  reversed.reverse
+end
+
+def kesha_maker(celebs)
+  celebs.each {|third| third[2] = "$"}
+end
+
+def find_a(fruits)
+  fruits.select {|letter| letter.start_with?("a")}
+end
+
+def sum_array(total)
+  total.inject {|sum, num| sum + num}
+end
+
+def add_s(want_s)
+  want_s.each_with_index.collect {|word, index| }
+end
